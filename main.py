b02dc1446873a7d5c025b837c9ed2cb1d73e9894
l4svl <kramarenkonikita17@gmail.com>
2022-08-30 03:35:14 +0300

    v 1.0

---
+++ main.py
@@ -0,0 +1,42 @@
+import random
+from datetime import datetime,timedelta
+import pyrogram.types
+from pyrogram import Client, filters
+from config import id, hash
+
+app_id = id
+app_hash = hash
+app = Client('account', app_id, app_hash)
+priton='-1001392765367'
+result=dict()
+keys=[]
+def zxc_killer():
+    i = 1000
+    while True:
+        app.send_message(priton, f'{i} -7 ={i - 7}')
+        i = i - 7
+        if i < 7:
+            app.send_message(priton,f'Больше нельзя вычитать,но Рома попуск')
+            return False
+def users_id():
+    a = app.get_chat_members(priton)
+    for i in a:
+        keys.append(i.user.username)
+        result[i.user.username]=i.user.id
+def Thanos():
+    app.send_message(priton,'ITS TIMEE FOR THANOS GAME!')
+    app.send_video(priton,r"C:\Users\User\PycharmProjects\TG_userBot\resource\thanos-finger-snap.mp4",caption='50% из вас будут уничтожены!')
+    for i in range(int(app.get_chat_members_count(priton)//2)):
+        try:
+            name=random.choice(keys)
+            keys.remove(name)
+            ud=result.get(name)
+            app.set_administrator_title(priton,ud,'Помер как лох!')
+            app.restrict_chat_member(priton,ud,pyrogram.types.ChatPermissions(),datetime.now()+timedelta(minutes=15))
+            app.send_message(priton,f'Пользователь {name} был успешно уничтожен, для баланса во вселенной!')
+        except Exception:
+            pass
+    app.send_message(priton,'Рома лох попуск ебаный (имхо) азазза')
+app.start()
+users_id()
+Thanos()
